ba1dedd7848c8313c07bb4b062ff4db5ae0a7d12
Mike Gardner <mjgardner@abitofhelp.com>
2023-12-23 18:23:55 -0700

    This is the initial commit of a working example.

--- build.rs
+++ build.rs
@@ -3,7 +3,11 @@ extern crate prost_build;
 fn main() {
     prost_build::compile_protos(
         &["proto/helloworld/helloworld.proto"],
-        &["proto/helloworld", "proto/googleapis", "grpc"],
+        &[
+            "proto/helloworld",
+            "proto/googleapis",
+            "grpc"
+        ],
     )
     .expect("failed to compile the proto files");
 }
